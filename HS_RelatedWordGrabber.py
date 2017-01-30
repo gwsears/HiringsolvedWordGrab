b72170fabd1be459011bf9a19affa0157bc979cb
gwsears <gerzel@foxpaws.net>
2017-01-30 14:05:38 -0500

    Finished iterating through to build dictionary.  Working on building output to csv.

--- HS_RelatedWordGrabber.py
+++ HS_RelatedWordGrabber.py
@@ -24,13 +24,16 @@ def getHiringSolvedRelatedWords(keyword,size,filter):
         r = requests.get(targetURL, auth=('user','pass'))
         relatedWords = r.json()
         print(relatedWords)
+        waiter(maxDelay)
     except Exception:
         print('Unable to get ' + keyword + '!')
+        waiter(maxDelay/2)
         return
     return {keyword:relatedWords}
 
 def loadKeywordList(file):
     # Takes local file path as input
+    # Returns a list of keywords
     openfile = open(file)
     openList = openfile.readlines()
     openfile.close()
@@ -39,7 +42,30 @@ def loadKeywordList(file):
         returnList = returnList + [item.strip()]
     return returnList
 
+def waiter(seconds):
+    # Add a delay between pulls in seconds
+    # If seconds is given as 0 or less then it adds no delay
+    if seconds >> 0:
+        waitT = randint(1,seconds)
+        print('Waiting ' + str(waitT) + ' seconds...')
+        sleep(waitT)
+
+# Start up dictionary to store our keywords and words related to them.
 keywordDict = {}
-for item in loadKeywordList(keywordInputFile):
+# Puts the keywords and related words into keywordDict.
+keywordList = loadKeywordList(keywordInputFile)
+for item in keywordList:
     keywordDict.update(getHiringSolvedRelatedWords(item,numSynonyms,filterHiringSolved))
+
+print("Here's your dictionary!")
 print(keywordDict)
+print(keywordDict[keywordList[0]])
+
+#def ouputCSVversion1(HSDictionary):
+#    csvfile = open(keywordOutputFile, 'w+', encoding='utf-8', newline='')
+#    outputWriter = csv.writer(csvfile, dialect='excel')
+#    toprow = ['keyword']
+#    for x in range(1, numSynonyms):
+#        toprow = toprow + ['related word', 'score']
+#    outputWriter.writerow(toprow)
+#    for keyword in HSDictionary:
